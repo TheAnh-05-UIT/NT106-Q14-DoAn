a3f5a6522e2d128ac09f3829060471ad2e8e8158
TheAnh-05 <nguyenquangtheanh2005@gmail.com>
2025-11-27 21:51:37 +0700

    pull code

--- NT106-Q14-DoAnGroup08/User Control/uc_Staff_Chat.Designer.cs
+++ NT106-Q14-DoAnGroup08/User Control/uc_Staff_Chat.Designer.cs
@@ -39,10 +39,11 @@
             // 
             this.lst_Chat.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lst_Chat.FormattingEnabled = true;
+            this.lst_Chat.ItemHeight = 20;
             this.lst_Chat.Location = new System.Drawing.Point(0, 0);
-            this.lst_Chat.Margin = new System.Windows.Forms.Padding(1);
+            this.lst_Chat.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.lst_Chat.Name = "lst_Chat";
-            this.lst_Chat.Size = new System.Drawing.Size(449, 376);
+            this.lst_Chat.Size = new System.Drawing.Size(674, 579);
             this.lst_Chat.TabIndex = 0;
             // 
             // panelBottom
@@ -50,10 +51,10 @@
             this.panelBottom.Controls.Add(this.txt_Chat);
             this.panelBottom.Controls.Add(this.btn_SendMessage);
             this.panelBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panelBottom.Location = new System.Drawing.Point(0, 376);
-            this.panelBottom.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.panelBottom.Location = new System.Drawing.Point(0, 579);
+            this.panelBottom.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.panelBottom.Name = "panelBottom";
-            this.panelBottom.Size = new System.Drawing.Size(449, 32);
+            this.panelBottom.Size = new System.Drawing.Size(674, 49);
             this.panelBottom.TabIndex = 1;
             // 
             // txt_Chat
@@ -61,31 +62,31 @@
             this.txt_Chat.Dock = System.Windows.Forms.DockStyle.Fill;
             this.txt_Chat.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txt_Chat.Location = new System.Drawing.Point(0, 0);
-            this.txt_Chat.Margin = new System.Windows.Forms.Padding(1);
+            this.txt_Chat.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txt_Chat.Name = "txt_Chat";
-            this.txt_Chat.Size = new System.Drawing.Size(372, 26);
+            this.txt_Chat.Size = new System.Drawing.Size(558, 35);
             this.txt_Chat.TabIndex = 0;
             // 
             // btn_SendMessage
             // 
             this.btn_SendMessage.Dock = System.Windows.Forms.DockStyle.Right;
-            this.btn_SendMessage.Location = new System.Drawing.Point(372, 0);
-            this.btn_SendMessage.Margin = new System.Windows.Forms.Padding(1);
+            this.btn_SendMessage.Location = new System.Drawing.Point(558, 0);
+            this.btn_SendMessage.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_SendMessage.Name = "btn_SendMessage";
-            this.btn_SendMessage.Size = new System.Drawing.Size(77, 32);
+            this.btn_SendMessage.Size = new System.Drawing.Size(116, 49);
             this.btn_SendMessage.TabIndex = 1;
             this.btn_SendMessage.Text = "Gửi";
             this.btn_SendMessage.UseVisualStyleBackColor = true;
             // 
             // uc_Staff_Chat
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.lst_Chat);
             this.Controls.Add(this.panelBottom);
-            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
             this.Name = "uc_Staff_Chat";
-            this.Size = new System.Drawing.Size(449, 408);
+            this.Size = new System.Drawing.Size(674, 628);
             this.panelBottom.ResumeLayout(false);
             this.panelBottom.PerformLayout();
             this.ResumeLayout(false);
