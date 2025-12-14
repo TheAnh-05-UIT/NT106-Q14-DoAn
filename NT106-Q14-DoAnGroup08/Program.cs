d609378e3d18e7fd47e3f5aae0fb67eff4b51f87
QuanBiXeChoi <24521454@gm.uit.edu.vn>
2025-12-14 10:14:01 +0700

    update

--- NT106-Q14-DoAnGroup08/Program.cs
+++ NT106-Q14-DoAnGroup08/Program.cs
@@ -19,11 +19,11 @@ namespace NT106_Q14_DoAnGroup08
         [STAThread]
         static void Main()
         {
-            ServerConfig.Configure("127.0.0.1", 8080);
+            ServerConfig.Configure("192.168.1.121", 8080);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
            // Application.Run(new frm_Login());
-            Application.Run(new frm_Staff("U002"));
+            Application.Run(new frm_LockScreen());
             Form nextform = DTO.UserSession.NextForm;
             if (nextform != null)
             {
--- NT106-Q14-DoAnGroup08/User Control/uc_Staff_Chat_Overview.Designer.cs
+++ NT106-Q14-DoAnGroup08/User Control/uc_Staff_Chat_Overview.Designer.cs
@@ -40,40 +40,43 @@
             this.panel1.Controls.Add(this.label1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(599, 502);
+            this.panel1.Size = new System.Drawing.Size(898, 784);
             this.panel1.TabIndex = 0;
             // 
             // listBox1
             // 
             this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(0, 33);
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(0, 47);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(599, 469);
+            this.listBox1.Size = new System.Drawing.Size(898, 737);
             this.listBox1.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.label1.Font = new System.Drawing.Font("Merriweather", 10F);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Padding = new System.Windows.Forms.Padding(10, 10, 0, 0);
-            this.label1.Size = new System.Drawing.Size(565, 33);
+            this.label1.Padding = new System.Windows.Forms.Padding(15, 16, 0, 0);
+            this.label1.Size = new System.Drawing.Size(809, 47);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nhấn đúp trong các người dùng dưới để khởi tạo cuộc trò chuyện:";
             // 
             // uc_Staff_Chat_Overview
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.Name = "uc_Staff_Chat_Overview";
-            this.Size = new System.Drawing.Size(599, 502);
+            this.Size = new System.Drawing.Size(898, 784);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.ResumeLayout(false);
